527339dba90b1fbb6ee67efa5ffceded31b8aaae
Steven Bitaxi <sbitaxi@gmail.com>
2020-06-20 23:32:15 -0400

    Removed GET method and added Person class

--- AzureFunction/AddNewPerson.cs
+++ AzureFunction/AddNewPerson.cs
@@ -14,7 +14,7 @@ namespace People.NewPerson
     {
         [FunctionName("AddNewPerson")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
@@ -22,8 +22,8 @@ namespace People.NewPerson
             string name = req.Query["name"];
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            dynamic data = JsonConvert.DeserializeObject<Person>(requestBody);
+            name = name ?? data?.FullName;
 
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
---
+++ AzureFunction/Person.cs
@@ -0,0 +1,21 @@
+namespace People.NewPerson
+{
+    public class Person
+    {
+        public string FirstName {get; set;}
+        public string LastName {get; set;}
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public string PostalCode { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                return FirstName + ' ' + LastName;
+            }
+        }
+    }
+}
