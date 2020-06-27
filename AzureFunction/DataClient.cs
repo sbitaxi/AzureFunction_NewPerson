ce0096615df40e231880239b0692af76ad465489
Steven Bitaxi <sbitaxi@gmail.com>
2020-06-27 00:01:38 -0400

    Added AppAuthentication for tokenauth and MSI

--- AzureFunction/DataClient.cs
+++ AzureFunction/DataClient.cs
@@ -33,6 +33,9 @@ namespace People.NewPerson
         private void InitConnection()
         {
             _connection = new SqlConnection(ConStr);
+            _connection.AccessToken = (new Microsoft.Azure.Services
+                            .AppAuthentication.AzureServiceTokenProvider())
+                            .GetAccessTokenAsync("https://database.windows.net/").Result;
         }
 
         private void InitCommand()
